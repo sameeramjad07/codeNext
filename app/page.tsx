068c7f05b03bc1788fd80e1bd0f6586dae5839e2
Sameer Amjad <proton7ve@gmail.com>
2024-12-10 10:47:37 +0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -39,6 +40,7 @@ export default function Home() {
             />
             Deploy now
           </a>
+          <Button>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -47,6 +49,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
